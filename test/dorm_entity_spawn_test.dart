ccf9898efbbe79dfd9f81e78119858e34d94caa7
Frank Pepermans <frankprms@hotmail.com>
2013-07-06 10:52:13 +0200

    added test

--- test/dorm_entity_spawn_test.dart
+++ test/dorm_entity_spawn_test.dart
@@ -2,23 +2,51 @@ library dorm_entity_spawn_test;
 
 import 'package:unittest/unittest.dart';
 import 'package:dorm/dorm.dart';
+import 'dart:json';
 
 main() {
   EntityManager manager = new EntityManager();
   Serializer serializer = new SerializerJson();
   EntityFactory<TestEntity> factory = new EntityFactory(handleConflictAcceptClient);
-  String rawData = '[{"id":1,"name":"Test","?t":"entities.testEntity"}]';
+  String rawDataA = '[{"id":1,"name":"Test A","?t":"entities.testEntity"}]';
+  String rawDataB = '[{"id":2,"name":"Test B","?t":"entities.testEntity"}]';
   
   manager.scan(TestEntity);
   
   test('Simple spawn test', () {
-    TestEntity entity = factory.spawn(serializer.incoming(rawData)).first;
-    TestEntity entityShouldBePointer = factory.spawn(serializer.incoming(rawData)).first;
+    TestEntity entity = factory.spawn(serializer.incoming(rawDataA)).first;
+    TestEntity entityShouldBePointer = factory.spawn(serializer.incoming(rawDataA)).first;
+    TestEntity entityShouldNotBePointer = factory.spawn(serializer.incoming(rawDataB)).first;
+    
+    entityShouldNotBePointer.cyclicReference = entity;
+    entity.cyclicReference = entityShouldNotBePointer;
+    
+    String outgoing = serializer.outgoing(<Entity>[entity]);
+    List<String> outgoingToComplexData = parse(outgoing);
     
     expect(entity.id, 1);
-    expect(entity.name, 'Test');
-    // both entities should be one and the same, not 2 seperate instances
+    expect(entity.name, 'Test A');
+    expect(entityShouldBePointer.id, 1);
+    expect(entityShouldBePointer.name, 'Test A');
+    expect(entityShouldNotBePointer.id, 2);
+    expect(entityShouldNotBePointer.name, 'Test B');
     expect((entity == entityShouldBePointer), true);
+    expect((entity != entityShouldNotBePointer), true);
+    expect((outgoing.length > 0), true);
+    
+    outgoingToComplexData.forEach(
+      (String entry) {
+        Map<String, dynamic> map = parse(entry);
+        
+        expect(map.containsKey('id'), true);
+        
+        if (!map.containsKey(SerializationType.POINTER)) {
+          expect(map.containsKey('name'), true);
+        }
+        
+        expect(map[SerializationType.ENTITY_TYPE], 'entities.testEntity');
+      }
+    );
   });
 }
 
@@ -51,7 +79,7 @@ class TestEntity extends Entity {
   Proxy<int> _id;
 
   static const String ID = 'id';
-  static const Symbol ID_SYMBOL = const Symbol('orm_domain.Person.id');
+  static const Symbol ID_SYMBOL = const Symbol('orm_domain.TestEntity.id');
 
   int get id => _id.value;
   set id(int value) => _id.value = notifyPropertyChange(ID_SYMBOL, _id.value, value);
@@ -65,10 +93,23 @@ class TestEntity extends Entity {
   Proxy<String> _name;
 
   static const String NAME = 'name';
-  static const Symbol NAME_SYMBOL = const Symbol('orm_domain.Person.name');
+  static const Symbol NAME_SYMBOL = const Symbol('orm_domain.TestEntity.name');
 
   String get name => _name.value;
   set name(String value) => _name.value = notifyPropertyChange(NAME_SYMBOL, _name.value, value);
+  
+  //---------------------------------
+  // cyclicReference
+  //---------------------------------
+
+  @Property(CYCLIC_REFERENCE_SYMBOL, 'name')
+  Proxy<TestEntity> _cyclicReference;
+
+  static const String CYCLIC_REFERENCE = 'cyclicReference';
+  static const Symbol CYCLIC_REFERENCE_SYMBOL = const Symbol('orm_domain.TestEntity.cyclicReference');
+
+  TestEntity get cyclicReference => _cyclicReference.value;
+  set cyclicReference(TestEntity value) => _cyclicReference.value = notifyPropertyChange(CYCLIC_REFERENCE_SYMBOL, _cyclicReference.value, value);
 
   //---------------------------------
   //
