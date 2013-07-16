6718f5e3b8fd85f286e99636f12722920902534c
Frank Pepermans <frankprms@hotmail.com>
2013-07-16 22:35:20 +0200

    improving key lookup

--- lib/src/core/entity_assembler.dart
+++ lib/src/core/entity_assembler.dart
@@ -331,9 +331,10 @@ class EntityAssembler {
   
   Entity _existingFromSpawnRegistry(String refClassName, _ProxyKey key, Entity entity) {
     Entity registeredEntity;
+    EntityScan scan = entity._scan;
     
     registeredEntity = _getSpawnRegistryForRefClassName(refClassName).entities.firstWhere(
-        (Entity lookup) => entity._scan.equalsBasedOnRefAndKey(lookup._scan),
+        (Entity lookup) => scan.equalsBasedOnRefAndKey(lookup._scan),
         orElse: () => null
     );
     
